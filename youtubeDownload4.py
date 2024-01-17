2f08ac9c7228e3db9eb7b0b4fae24d0a7b24422f
Im-shamo <samuelkwok2007@gmail.com>
2024-01-17 20:13:58 +0800

    ahahahahaahadhsfdahfdsahfdassaf

--- youtubeDownload4.py
+++ youtubeDownload4.py
@@ -1,10 +1,11 @@
 from pytube import YouTube, Playlist
 import os
 
-#test
+# test
+
 
 def mode_select(modes: dict) -> str:
-    
+
     question = f"\nPlease select mode (1 - {len(modes)})"
     for i, name in enumerate(modes):
         question += f"\n({i + 1}) {name}"
@@ -25,16 +26,26 @@ def mode_select(modes: dict) -> str:
         except ValueError:
             print("\nMode not found. Please try again")
 
+
 def seconds_to_min(seconds):
     minutes = seconds // 60
     remaining_seconds = seconds % 60
     return f"{minutes}:{remaining_seconds:02d}"
 
 
+def check_num_range(n, start, end):
+    if start <= n and end >= n:
+        return True
+    else:
+        return False
+
+# handles user inputs
+
+
 def get_input(question, default=None):
 
     if default is not None:
-        question += f". Default is {str(default)}"
+        question += f". Default is {str(default)}."
 
     question = "\n" + question + "\n>"
     while True:
@@ -45,6 +56,7 @@ def get_input(question, default=None):
 
         if default is not None:
             return default
+
 
 def get_confirm(question, default=None):
 
@@ -58,6 +70,7 @@ def get_confirm(question, default=None):
 
         elif answer.lower().strip() in ["no", "n"]:
             return False
+
 
 def get_download_dir():
 
@@ -78,13 +91,61 @@ def get_links():
     links = get_input("Enter links sperated by space")
     return links.split()
 
-def get_videos_modes(videos, download_dir):
+
+def get_selection(number_of_things, array) -> list:
+
+    selection = []
+    answer = get_input("Enter selection (1-10 5)",
+                       default="all").lower().strip()
+
+    if answer == "all":
+        return array
+
+    answer = answer.split()
+
+    print(answer)
+
+    for x in answer:
+        try:
+            if len(x.split("-")) == 2:
+
+                start, end = x.split("-")
+
+                start = int(start)
+                end = int(end)
+
+                if (start < end and check_num_range(start, 1, number_of_things) and
+                check_num_range(end, 1, number_of_things)):
+
+                    selection.extend([elem for elem in array[start-1:end] if elem not in selection])
+
+            elif x.isdigit():
+
+                n = int(x)
+
+                if check_num_range(n, 1, number_of_things):
+                    if not array[n-1] in selection:
+                        selection.append(array[n-1])
+
+        except ValueError:
+            pass
+
+    return selection
+
+
+# handles downloading
+
+def get_videos_modes():
     MODES = {
+        "Highest progressive video": download_highest_resolution,
         "By itag": download_by_itag,
-        "Highest progressive resolution": download_highest_resolution,
+        "Filter Audio": filter_audio,
+        "Filter Video": filter_video,
+        "Filter Adaptive": filter_adaptive
     }
 
-    MODES[mode_select(MODES)](videos, download_dir)
+    return MODES[mode_select(MODES)]
+
 
 def download_highest_resolution(videos: list, download_dir: str):
 
@@ -92,28 +153,61 @@ def download_highest_resolution(videos: list, download_dir: str):
     download_streams(streams, download_dir)
 
 
-def download_by_itag(videos: list, download_dir: str):
+def download_by_itag(videos: list, download_dir: str, itag=None, print_info=True):
 
-    [print_stream_data(video) for video in videos]
+    if print_info:
+        [print_stream_data(video) for video in videos]
 
-    itag = int(get_input("Enter itag"))
+    if itag is None:
+        itag = int(get_input("Enter itag"))
 
     streams = [video.streams.get_by_itag(itag) for video in videos]
     download_streams(streams, download_dir)
 
 
+def filter_audio(videos: list, download_dir: str):
+
+    for video in videos:
+        print("\nTitle: ", video.title)
+        [print(line) for line in video.streams.filter(only_audio=True)]
+
+    download_by_itag(videos, download_dir, print_info=False)
+
+
+def filter_video(videos: list, download_dir: str):
+
+    for video in videos:
+        print("\nTitle: ", video.title)
+        [print(line) for line in video.streams.filter(only_video=True)]
+
+    download_by_itag(videos, download_dir, print_info=False)
+
+
+def filter_adaptive(videos: list, download_dir: str):
+
+    for video in videos:
+        print("\nTitle: ", video.title)
+        [print(line) for line in video.streams.filter(adaptive=True)]
+
+    download_by_itag(videos, download_dir, print_info=False)
+
+
 def download_streams(streams: list, download_dir: str):
 
     failed_downloads = []
+
+    if get_confirm("Display filesize", default="n"):
+        print_streams_filesize(streams)
+
     if get_confirm("Download videos?"):
 
-        for i , stream in enumerate(streams):
+        for i, stream in enumerate(streams):
             print(f"\n{i+1}: Downloading {stream.title}")
 
             try:
                 stream.download(download_dir)
                 print(f"{i+1}: Downloaded {stream.title}")
-            
+
             except Exception as e:
                 print(f"{i+1}: Failed to download {stream.title}. Error {e}")
                 failed_downloads.append(stream)
@@ -121,20 +215,30 @@ def download_streams(streams: list, download_dir: str):
         if failed_downloads and get_confirm("Retry download", "n"):
             download_streams(failed_downloads, download_dir)
 
-
     else:
         print("Fail to Download")
-    
-
-def print_stream_data(video):
-
-    print("\n",video.title)
-    [print(line) for line in video.streams]
 
 
-def shamo_videos(videos = None|list):
+# handles printing info
 
-    while not(videos):
+def print_streams_filesize(streams: list):
+
+    [print(f"{i+1:^4}| Filesize: {stream.filesize_mb}MB {'':^4}| Title: {stream.title}")
+     for i, stream in enumerate(streams)]
+
+
+def print_streams_info(videos: list):
+
+    for video in videos:
+        print("\nTitle: ", video.title)
+        [print(line) for line in video.streams]
+
+# the modes
+
+
+def shamo_videos(videos=None):
+
+    while not (videos):
         try:
             videos = [YouTube(videos) for videos in get_links()]
             break
@@ -144,13 +248,14 @@ def shamo_videos(videos = None|list):
 
     download_dir = get_download_dir()
 
-    [print(f"{i+1} | Author: {video.author:<40} | Video title: {video.title:<40} | Length: {seconds_to_min(video.length)}")
-    for i, video in enumerate(videos)]
+    [print(f"{i+1} | Author: {video.author:<20} | Video title: {video.title:<40} | Length: {seconds_to_min(video.length)}")
+     for i, video in enumerate(videos)]
 
-    get_videos_modes(videos, download_dir)
+    get_videos_modes()(videos, download_dir)
 
 
 def shamo_playlist():
+
     while True:
         try:
             playlists = [Playlist(videos) for videos in get_links()]
@@ -158,8 +263,9 @@ def shamo_playlist():
         except Exception as e:
             print(e)
 
+    [print(f"{i+1} | Author: {playlist.author:<20} | Playlist title: {playlist.title:<40}")
+     for i, playlist in enumerate(playlists)]
 
-    
 
 def main():
 
@@ -170,5 +276,15 @@ def main():
 
     MODES[mode_select(MODES)]()
 
+
+def test():
+    array = ["a", "b", "c", "d", "e", 1, 2, 3, 4]
+
+    selection = get_selection(len(array), array)
+
+    print(selection)
+
+
 if __name__ == "__main__":
-    main()
+    # main()
+    test()
