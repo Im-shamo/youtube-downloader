432925955817e68157294e557aff4e2818f52a5f
Shamokwok <samuelkwok2007@gmail.com>
2024-04-18 23:36:53 +0800

    completed playlists cli

--- youtube_download/__main__.py
+++ youtube_download/__main__.py
@@ -1,7 +1,7 @@
 from user_input import mode_select
 from shamo_videos import shamo_videos
 from shamo_playlist import shamo_playlist
-from cli import video_cli
+from cli import video_cli, playlist_cli, print_playlists_details, print_videos_details
 import argparse
 from sys import argv
 from os import path
@@ -16,17 +16,20 @@ def get_args() -> vars:
     url_group.add_argument("--playlist-url", type=str, nargs="+", help="playlist url(s)")
 
 
-    options_group = parser.add_mutually_exclusive_group()
-    options_group.add_argument("--high-res", default=False, action="store_true", help="Download highest resolution")
-    options_group.add_argument("--high-audio", default=False, action="store_true", help="Download hightest quality audio")
-    options_group.add_argument("--itag", type=int, help="Download by itag: int")
+    download_options_group = parser.add_mutually_exclusive_group()
+    download_options_group.add_argument("--high-res", default=False, action="store_true", help="Download highest resolution")
+    download_options_group.add_argument("--high-audio", default=False, action="store_true", help="Download hightest quality audio")
+    download_options_group.add_argument("--itag", type=int, help="Download by itag: int")
+
+    parser.add_argument("--print-info", "-p", default=False, action="store_true", help="print info about videos or playlists")
 
 
     # https://stackoverflow.com/questions/15935092/creating-mutually-inclusive-positional-arguments-with-argparse
     argvs = parser.parse_args()
     if argvs.video_url or argvs.playlist_url:
-        if not (argvs.high_res or argvs.high_audio or argvs.itag):
+        if not (argvs.high_res or argvs.high_audio or argvs.itag) and not argvs.print_info:
             print("must at least have one download option: --high-res --high-audio --itag")
+            quit()
     return argvs
 
 
@@ -41,9 +44,24 @@ def main():
         }
 
         MODES[mode_select(MODES)]()
+        quit()
 
     elif argvs.video_url:
-        video_cli(argvs.video_url, argvs.output_path, argvs.verbose, high_res=argvs.high_res, high_audio=argvs.high_audio, itag=argvs.itag)
+        if argvs.print_info:
+            print_videos_details(argvs.video_url)
+        else:
+            video_cli(argvs.video_url, argvs.output_path, argvs.verbose, high_res=argvs.high_res, high_audio=argvs.high_audio, itag=argvs.itag)
+        quit()
+
+    elif argvs.playlist_url:
+        if argvs.print_info:
+            print_playlists_details(argvs.playlist_url)
+        else:
+            playlist_cli(argvs.playlist_url, argvs.output_path, argvs.verbose, high_res=argvs.high_res, high_audio=argvs.high_audio, itag=argvs.itag)
+        quit()
+
+
+
 
 
 if __name__ == "__main__":
--- youtube_download/cli.py
+++ youtube_download/cli.py
@@ -2,13 +2,16 @@ from videos_download import VideosDownload
 from utility import *
 from pytube import YouTube, Playlist, exceptions
 from os import path
+from typing import Optional
 
 
 def video_cli(
-        url: list[str],
+        urls: list[str],
         ouput_path: str,
         verbose: bool,
-        **options
+        high_res: bool = False,
+        high_audio: bool = False,
+        itag: Optional[int] = None
 ) -> None:
 
     ouput_path = path.abspath(ouput_path)
@@ -17,37 +20,105 @@ def video_cli(
 
     while True:
         try:
-            videos = [YouTube(url) for url in url]
+            videos = [YouTube(link) for link in urls]
             break
         except Exception as e:
             if verbose:
                 print(e)
 
     if verbose:
-        output_text = ""
-        for i, video in enumerate(videos):
-            author = video.author[:10] + \
-                "..." if len(video.author) > 10 else video.author
-            title = video.title
-            length = seconds_to_min(video.length)
-            num = str(i+1)
-
-            output_text += f"{num:<4} | Author: {author:<15} | Length: {length:^5} | Video title: {title}\n"
-
-        print(output_text)
+        print_videos_details(videos=videos)
 
     download = VideosDownload(videos, ouput_path)
-    if options["high_res"]:
+    if high_res:
         download.highest_resolution()
-    elif options["high_audio"]:
+    elif high_audio:
         download.audio_only()
-    elif options["itag"]:
-        download.itag(options["itag"])
+    elif itag:
+        download.itag(itag)
 
     if verbose:
         download.print_streams_filesize()
     download.download(print_info=verbose)
 
 
-def playlist_cli() -> None:
-    pass
+def playlist_cli(
+        urls: list[str],
+        output_path: str,
+        verbose: bool,
+        high_res: bool = False,
+        high_audio: bool = False,
+        itag: Optional[int] = None
+) -> None:
+
+    ouput_path = path.abspath(ouput_path)
+    if not path.exists(ouput_path):
+        raise FileNotFoundError
+
+    while True:
+        try:
+            playlists = [Playlist(link) for link in urls]
+            break
+
+        except Exception as e:
+            print(e)
+
+    if verbose:
+        print_playlists_details(playlists=playlists)
+
+    for playlist in playlists:
+        videos = playlist.videos
+        if verbose:
+            print_videos_details(videos=videos)
+        download = VideosDownload(videos, output_path)
+        if high_res:
+            download.highest_resolution()
+        elif high_audio:
+            download.audio_only()
+        elif itag:
+            download.itag(itag)
+
+        if verbose:
+            download.print_streams_filesize()
+        download.download(verbose)
+
+
+def print_videos_details(
+        urls: Optional[list[str]] = None,
+        videos: Optional[list[YouTube]] = None,
+) -> None:
+
+    if urls:
+        try:
+            videos = [YouTube(url) for url in urls]
+
+        except Exception as e:
+            print(e)
+
+    output_text = ""
+    for i, video in enumerate(videos):
+        author = video.author[:15] + \
+            "..." if len(video.author) > 15 else video.author
+        title = video.title
+        length = seconds_to_min(video.length)
+        num = str(i+1)
+
+        output_text += f"{num:<4} | Author: {author:<15} | Length: {length:^5} | Video title: {title}\n"
+
+    print(output_text)
+
+
+def print_playlists_details(
+    urls: Optional[list[str]] = None,
+    playlists: Optional[list[Playlist]] = None
+) -> None:
+
+    if urls:
+        try:
+            playlists = [Playlist(url) for url in urls]
+
+        except Exception as e:
+            print(e)
+
+    [print(f"{i+1} | Playlist title: {playlist.title:<40} | No. vidoes: {playlist.length}")
+     for i, playlist in enumerate(playlists)]
